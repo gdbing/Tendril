7c16153f0c82d639db4a68db9234a646c278a431
Graham Bing <grrraham@gmail.com>
2023-05-18 23:51:18 -0700

    Settings sheet

--- Tendril.swiftpm/ContentView.swift
+++ Tendril.swiftpm/ContentView.swift
@@ -3,14 +3,20 @@ import SwiftChatGPT
 
 struct ContentView: View {
     private let chatGPT = ChatGPT(key: "")
-    private var settings: Settings = Settings()
     
     @Binding var document: TextDocument
+    
+    @State private var settings: Settings = Settings()
+    @State private var showingSettings: Bool = false
     
     var body: some View {
         VStack {
             DocumentView(text: $document.text)
             HStack {
+                Button("settings", action: {
+                    self.showingSettings = true
+                })
+                .buttonStyle(.bordered)
                 Button("append uneaten", action: {
                     GPTify()                    
                 })
@@ -29,11 +35,26 @@ struct ContentView: View {
                 self.chatGPT.model = newValue ? "gpt-4" : "gpt-3.5-turbo"
             })
             .onChange(of: settings.temperature, perform: { newValue in
-                self.chatGPT.temperature = Float(temperature)
+                self.chatGPT.temperature = Float(newValue)
             })
             .onChange(of: settings.systemMessage, perform: { newValue in
-                self.chatGPT.systemMessage = systemMessage
+                self.chatGPT.systemMessage = newValue
             })
+            .sheet(isPresented: $showingSettings) {
+//                NavigationView {
+                    SettingsView(settings: $settings)
+                        .padding()
+//                }
+                        .toolbar {
+                            ToolbarItem(placement: .primaryAction) {
+                                Button(action: {
+                                    showingSettings = false
+                                }) {
+                                    Text("Done").fontWeight(.semibold)
+                                }
+                            }
+                        }
+            }
         }
     }
     
--- Tendril.swiftpm/Settings.swift
+++ Tendril.swiftpm/Settings.swift
@@ -5,5 +5,16 @@ struct Settings {
     @AppStorage("systemMessage") var systemMessage: String = "You are a helpful assistant"
     @AppStorage("temperature") var temperature: Double = 0.7
     @AppStorage("isGPT4") var isGPT4: Bool = false
+}
 
+struct SettingsView: View {
+    @Binding var settings: Settings
+    
+    var body: some View {
+        TextField("API Key", text: settings.$apiKey)
+        TextEditor(text: settings.$systemMessage)
+            .border(Color.secondary, width: 1)
+        Slider(value: settings.$temperature)//, in: 0...1.6, step: 0.1)
+        Toggle("GPT4", isOn: settings.$isGPT4)
+    }
 }
